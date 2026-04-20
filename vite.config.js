e4e8b94429fc5809758736fbf91705b5dd5efd87
whiplashFire <3232069085@qq.com>
2026-04-20 19:57:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ych-checker/',
+  base: '/perfume-test/',
   plugins: [react(), tailwindcss()],
 })

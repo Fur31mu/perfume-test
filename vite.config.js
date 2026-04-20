fbf4b7b4ceac907d1598ba8c3260348668937548
whiplashFire <3232069085@qq.com>
2026-04-20 19:55:38 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/fur31mu/',
+  base: '/ych-checker/',
   plugins: [react(), tailwindcss()],
 })
